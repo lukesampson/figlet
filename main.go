021c5fa29ae57d6b187dfb9b937ba7490140340b
Luke Sampson <me@lukesampson.com>
2013-07-27 20:19:21 +1000

    stub some functions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 const (
@@ -37,9 +38,37 @@ func getChar(c rune, f font) []string {
 	 return l
 }
 
-//func getLines(f font, msg string) [][]string {
+// returns true if the word could be added to the line
+func addWord(f font, word string, line []string) bool {
+	return false
+}
 
-//}
+// Gets the next line that will fit in allowed width
+func nextLine(f font, msg string, width int) ([]string, string) {
+	line := make([]string, f.header.charheight)
+	words := strings.Split(msg, " ")
+	for i, word := range words {
+		if addWord(f, word, line) {
+			msg = strings.Join(words[i+1:], " ")
+		} else if i == 0 { // word longer than line
+			panic("forced word break not implemented")
+		} else {
+			break 
+		}
+	}
+	return line, msg
+}
+
+
+func getLines(f font, msg string, width int) [][]string {
+	lines := make([][]string, 0, 1) // make room for at least one line
+	for len(msg) > 0 {
+		var line []string
+    	line, msg = nextLine(f, msg, width)
+    	lines = append(lines, line)
+    }
+    return lines
+}
 
 func main() {
 	fontsdir, err := findFonts()
