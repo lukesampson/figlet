167df4be7bd69e0caceb0fc13148f9c1854ec4e4
Luke Sampson <me@lukesampson.com>
2013-07-29 13:13:41 +1000

    test functions to enumerate smush modes and all distinguishable modes

--- layout_test.go
+++ layout_test.go
@@ -1,26 +1,33 @@
 package main
 
-func validFlags() int[] {
-	flags := make(int[], 0, 36)
+import "testing"
+
+func validSmushModes() []int {
+	modes := make([]int, 0, 36 + 1)
+	modes = append(modes, 0)
 	for i := 1; i <= 128; i *= 2 {
-		append(flags, i)
+		modes = append(modes, i)
 		for j := 1; j < i; j *= 2 {
-			append(flags, i+j)
+			modes = append(modes, i+j)
 		}
 	}
+	return modes
 }
 
-func TestValidFlags(t * testing.T) {
-	for _, f := range(validFlags()) {
-		t.log(f)
+func smushModes() []int {
+	modes := make([]int, 8+1)
+	modes[0] = 0
+	for i := uint(0); i < 8; i++ {
+		modes[i+1] = 1 << i
 	}
+	return modes
 }
 
 func TestSmushem_LChrEmpty_Always_Returns_Rchr(t *testing.T) {
 	lchr := ' '
 	rchrs := []rune { 'a', '!', '$' }
 	for _, rchr := range rchrs {
-		if x:= smushem(lchr, rchr); x != rchr {
+		if x, ok := smushem(lchr, rchr, 0); ok && x != rchr {
 			t.Errorf("smushem(%v, %v) = %v, want %v", lchr, rchr, x, rchr)
 		}
 	}
